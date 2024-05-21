8d2bbf28d6c68b314df9cdeb22e16d00f6e5a663
kili1 <kilian168@gmail.com>
2024-05-21 19:39:34 +0200

    make it better

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,7 @@
+const Schüler = require('../models/UserSchüler');
+const Lehrer = require('../models/UserLehrer');
+
+
 module.exports.register_get = (req, res) => {
     res.render('register');
 }
@@ -6,11 +10,18 @@ module.exports.login_get = (req, res) => {
     res.render('login');
 }
 
-module.exports.register_post = (req, res) => {
-
-    res.send(req.body.name)
+module.exports.register_post = async (req, res) => {
+    const {username, name, lehrer, password} = req.body;
+    try {
+        const schüler = await User.create({ username, name, lehrer, password});
+        res.status(201).json(user);
+    }
+    catch(err) {
+        console.log(err);
+        res.status(400).send('error, user not created');
+    }
 }
 
-module.exports.login_post = (req, res) => {
-    res.send(req.body.password);
+module.exports.login_post = async (req, res) => {
+    
 }
